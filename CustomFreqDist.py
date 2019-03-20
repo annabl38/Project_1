1271e1c7c621a23891694a4988cbe2e517803a98
jchung243 <jchung243@gmail.com>
2019-03-19 20:53:15 -0400

    fixed custom class to return pylab object and used savefig to save images.

--- CustomFreqDist.py
+++ CustomFreqDist.py
@@ -1,10 +1,6 @@
 import nltk
-# You may have to do this if you haven't yet.
-# nltk.download('punkt')
-# This is one of the libraries/functions that the nltk developers used in their code.
 from six import text_type
 
-# This is another custom function they wrote thats needed in the plotting method.
 def _get_kwarg(kwargs, key, default):
     if key in kwargs:
         arg = kwargs[key]
@@ -13,24 +9,8 @@ def _get_kwarg(kwargs, key, default):
         arg = default
     return arg
 
-# Here we create a custom class that extends the FreqDist class that nltk provides.
-# This essentially makes a copy of the FreqDist class, and then you can modify it with your own code.
 class CustomFreqDist(nltk.probability.FreqDist):
-	# Here we create our own plotting function.
-	# This is where you can modify the chart with whatever you want.
     def custom_plot(self, *args, **kwargs):
-        """
-        Plot samples from the frequency distribution
-        displaying the most frequent sample first.  If an integer
-        parameter is supplied, stop after this many samples have been
-        plotted.  For a cumulative plot, specify cumulative=True.
-        (Requires Matplotlib to be installed.)
-
-        :param title: The title for the graph
-        :type title: str
-        :param cumulative: A flag to specify whether the plot is cumulative (default = False)
-        :type title: bool
-        """
         try:
             from matplotlib import pylab
         except ImportError:
@@ -66,6 +46,5 @@ class CustomFreqDist(nltk.probability.FreqDist):
         pylab.xticks(range(len(samples)), [text_type(s) for s in samples], rotation=90)
         pylab.xlabel("Words")
         pylab.ylabel(ylabel)
-		# Here I added the title.
-        pylab.show()
+        return pylab
 
